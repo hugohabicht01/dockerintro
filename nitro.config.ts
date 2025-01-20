4fbd292565300583d7123eb0d022d2d16c77f358
hugohabicht01 <anon@example.org>
2025-01-20 22:53:22 +0000

    whatever

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,6 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-01-20"
+  compatibilityDate: "2025-01-20",
+  serveStatic: true,
 });
